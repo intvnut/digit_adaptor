bfd2c2d00e5e112df85a6d570115f76489e10683
Joe Zbiciak <intvnut@gmail.com>
2022-12-24 02:17:17 -0800

    Add a radix 5 test

--- digit_adaptor_test.cc
+++ digit_adaptor_test.cc
@@ -70,7 +70,72 @@ int main() {
     auto cda = digit_adaptor{cu};
 
     for (int i = 0; i < 7; ++i) {
-      std::cout 
+      std::cout
+        << "mda[" << i << "] == cda[" << i << "]? "
+        << (mda[i] == cda[i]) << '\n';
+    }
+
+    for (int i = 0; i < 7; ++i) {
+      std::cout
+        << "mda[" << i << "] + cda[" << i << "]? "
+        << (mda[i] + cda[i]) << '\n';
+    }
+  }
+
+  {
+    auto u = 8675309u;
+    auto da = digit_adaptor<unsigned, 5>{u};
+
+    output_digits(da, u);
+    std::reverse(da.begin(), da.end());
+    output_digits(da, u);
+    std::sort(da.begin(), da.end());
+    output_digits(da, u);
+    da[4]++;
+    output_digits(da, u);
+    --da[4];
+    output_digits(da, u);
+    da[0]=1;
+    output_digits(da, u);
+    auto ri = da.rbegin();
+    *ri++ = 1;
+    *ri++ = 2;
+    *ri++ = 3;
+    *ri++ = 4;
+    output_digits(da, u);
+  }
+
+  {
+    auto i = -8675309;
+    auto da = digit_adaptor<int, 5>{i};
+
+    output_digits(da, i);
+    std::reverse(da.begin(), da.end());
+    output_digits(da, i);
+    std::sort(da.begin(), da.end());
+    output_digits(da, i);
+    da[4]++;
+    output_digits(da, i);
+    --da[4];
+    output_digits(da, i);
+    da[0]=1;
+    output_digits(da, i);
+    auto ri = da.rbegin();
+    *ri++ = 1;
+    *ri++ = 2;
+    *ri++ = 3;
+    *ri++ = 4;
+    output_digits(da, i);
+  }
+
+  {
+    auto       mu = 8675309;
+    const auto cu = 8675319;
+    auto mda = digit_adaptor<int, 5>{mu};
+    auto cda = digit_adaptor<const int, 5>{cu};
+
+    for (int i = 0; i < 7; ++i) {
+      std::cout
         << "mda[" << i << "] == cda[" << i << "]? "
         << (mda[i] == cda[i]) << '\n';
     }
